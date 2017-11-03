359f3a17f5bcc0ba8651bc66417db52ffdf233a7
tyler.coy <tyler.coy@wsu.edu>
2017-11-03 12:46:23 -0700

    Tyler-Commit Test

--- src/tank/StateMain.java
+++ src/tank/StateMain.java
@@ -20,6 +20,7 @@ public class StateMain extends BasicGameState
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException
 		{
 		g.drawString("Tank", 100, 100);
+		g.drawString("Tyler Test",100,100);
 		}
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException
