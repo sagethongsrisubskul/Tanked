2e6d9dd8dbb69cf7a0341c8f5363dc4cfde23876
Benjamin DeCamp <benjamin.decamp@wsu.edu>
2017-11-06 11:25:30 -0800

    Moves resource files into src folder for easiler project management. Edits paths in Filenames class

--- src/tank/Filenames.java
+++ src/tank/Filenames.java
@@ -3,20 +3,20 @@ package tank;
 public class Filenames
 	{
 	/// Title & logo:
-	public final static String title = "Title.png";
-	public final static String logo = "Logo.png";
+	public final static String title = "res/Title.png";
+	public final static String logo = "res/Logo.png";
 	/// Navigation buttons:
-	public final static String navFF = "Navigation_ff.png";
-	public final static String navRew = "Navigation_rew.png";
-	public final static String navNext = "Navigation_next.png";
-	public final static String navPrev = "Navigation_prev.png";
-	public final static String navHome = "Navigation_home.png";
-	public final static String navBack = "Navigation_back.png";
+	public final static String navFF = "res/Navigation_ff.png";
+	public final static String navRew = "res/Navigation_rew.png";
+	public final static String navNext = "res/Navigation_next.png";
+	public final static String navPrev = "res/Navigation_prev.png";
+	public final static String navHome = "res/Navigation_home.png";
+	public final static String navBack = "res/Navigation_back.png";
 	/// Sounds:
-	public final static String buttonClick = "buttonClick.wav";
+	public final static String buttonClick = "res/buttonClick.wav";
 	/// Buttons:
-	public final static String buttonRectangle = "ButtonRectangle.png";
-	public final static String buttonSquare = "ButtonSquare.png";
+	public final static String buttonRectangle = "res/ButtonRectangle.png";
+	public final static String buttonSquare = "res/ButtonSquare.png";
 	/// Mini maps:
-	public final static String miniMap[] = {"miniMap1.jpg", "miniMap2.jpg", "miniMap3.jpg"};
+	public final static String miniMap[] = {"res/miniMap1.jpg", "res/miniMap2.jpg", "res/miniMap3.jpg"};
 	}
