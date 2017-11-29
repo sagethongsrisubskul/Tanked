9c265c5c842af304948e7545f73c18b741124dae
Samuel <samuelriesterer@hotmail.com>
2017-11-29 11:35:36 -0800

    Added tilesets for map2 and map3

--- src/tank/Filenames.java
+++ src/tank/Filenames.java
@@ -25,6 +25,7 @@ public class Filenames
 	/// Maps:
 	public final static String maps[] = {"res/Map1.tmx", "res/Map2.tmx", "res/Map3.tmx"};
 	public final static int mapSize[][] = {{640,640},{800,544},{1600,1088}};
+
 //	public final static String tilesets[] = {"res/Maptiles.tsx", "res/tankMasterTL.tsx"};
 //	public final static String tilesetsMap3[] = {"res/tankMasterTL.tsx", "res/tankMasterTR.tsx", "res/tankMasterBL.tsx", "res/tankMasterBR.tsx"};
 //	public final static String tilesetImages[] = {"res/tiles.png", "res/tankMasterTL.png", "res/tankMaster2.png"};
