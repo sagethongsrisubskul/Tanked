3696f66dd5178cfab542d48c38561b712407b057
Sage Thongsrisubskul <s.thongsrisubskul@wsu.edu>
2017-12-08 20:41:54 -0800

    initial projectile functionality

--- src/tank/projectile.java
+++ src/tank/projectile.java
@@ -7,18 +7,30 @@ public class projectile extends Entity{
 	private Vector acceleration;
 	private Vector worldpos;
 	private double angle;
+	private int lifetime;
 
 	//x,y=coordinates, r=angle,mv=muzzle velocity, l=lifetime
 	public projectile(final float x, final float y, final double r, final float mv, final int l){
 		super(x,y);
 		
-		angle=(180f/Math.PI)*Math.atan2(x,-y);
+		angle=r;
+
+		if(mv!=0){
+			//is shot
+			ResourceManager.loadImage(Filenames.shot);
+			addImageWithBoundingBox(ResourceManager.getImage(Filenames.shot));
+			lifetime=5000;
+		}else{
+			//is mine
+			lifetime=30000;
+		}
+		this.setRotation(angle);
 		
 		velocity=new Vector(mv*(float)Math.sin(angle*(Math.PI/180.0f)),-mv*(float)Math.cos(angle*(Math.PI/180.0f)));
 		
 	}
 	
-	public void update(final int delta,int i){
+	public void update(final int delta){
 		translate(velocity.scale(delta*0.1f));
 	}
 	
