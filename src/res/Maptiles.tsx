409c0b1dc63f17a8a4f504bb12db82f9b43b301a
Benjamin DeCamp <benjamin.decamp@wsu.edu>
2017-12-14 13:17:04 -0800

    Fixes maptiles file

--- src/res/Maptiles.tsx
+++ src/res/Maptiles.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tiles" tilewidth="64" tileheight="64" tilecount="288" columns="12">
- <image source="../Downloads/tiles.png" width="768" height="1536"/>
+ <image source="tiles.png" width="768" height="1536"/>
  <tile id="43">
   <properties>
    <property name="Solid" type="bool" value="true"/>
